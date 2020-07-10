b1c8150128c6670f9f9cf047fda7d5d8eb9acf49
Charvi04 <66305846+Charvi04@users.noreply.github.com>
2020-07-10 11:55:51 +0530

    Update PaperBall.js

--- PaperBall.js
+++ PaperBall.js
@@ -1,26 +1,22 @@
-class PaperBall {
+class Ball2 {
     constructor(x,y,width,height) {
       var options = {
           isStatic: false,
-          friction:0.5,
+          density:1.2,
           restitution:0.2,
-          density:1.2
+          friction:0.5
 
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
-      this.image = loadImage("paper.png");
       this.width = width;
       this.height = height;
-      
+      this.image = loadImage("paper.png");
       World.add(world, this.body);
-     
     }
     display(){
-      
-      
-      fill("yellow");
-      
+      fill("brown");
       imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+      
+      image( this.image,100,300,50,50);
     }
   };
